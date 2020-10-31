63bbbd561634d75a6d98653f8f7a748dc0ee52cb
andreinab9 <71570901+andreinab9@users.noreply.github.com>
2020-10-31 20:54:18 -0300

    Add files via upload

---
+++ data/CerroArco_17.js
@@ -0,0 +1,8 @@
+var json_CerroArco_17 = {
+"type": "FeatureCollection",
+"name": "CerroArco_17",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": null, "TIPO": "Cerro Arco" }, "geometry": { "type": "Point", "coordinates": [ -68.930958384241961, -32.84254994546508 ] } }
+]
+}
